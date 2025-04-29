ba4db51a30cd19fc3116c81a0239f38cf1098dd6
Bestiario360 <Bestiario360@gmail.com>
2025-04-29 00:21:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+function obtenerFechaActual() {
+    const hoy = new Date();
+    const dia = hoy.getDate().toString().padStart(2, '0');
+    const mes = (hoy.getMonth() + 1).toString().padStart(2, '0');
+    const año = hoy.getFullYear();
+    return `${dia}/${mes}/${año}`;
+}
 
 async function validarCodigo() {
     const codigoIngresado = document.getElementById('codigo').value.trim().toUpperCase();
